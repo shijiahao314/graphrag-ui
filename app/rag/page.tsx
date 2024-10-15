b9dfd212e84364b04471bd171bfbbc8cfc4466e7
shijiahao <shijiahao314@foxmail.com>
2024-10-15 17:40:01 +0800

    first commit

---
+++ app/rag/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <div className="flex">
+      <div className="w-full h-full flex flex-row mx-4 my-3">
+        <div className="w-full flex">
+          <label className="">
+            KB 知识库
+          </label>
+        </div>
+        <div className="w-full flex">
+          <label className="">
+            Data 数据
+          </label>
+        </div>
+      </div>
+      <div className="">
+
+      </div>
+    </div>
+  );
+}
